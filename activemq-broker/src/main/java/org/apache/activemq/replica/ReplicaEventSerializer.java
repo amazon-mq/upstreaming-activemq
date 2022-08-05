b326f21141fcf68c5041621b59fc1526bd436005
Dmytro Chebotarskyi <chebod@amazon.com>
2022-08-05 13:28:09 -0700

    Revert "[PERFORMANCE TEST COMMIT] adding logging to indicate average time for operations"
    This reverts commit 1bf47f90863bafbd2554712f8ea81cbd17bfba9d.
    
    cr: https://code.amazon.com/reviews/CR-73808963

--- activemq-broker/src/main/java/org/apache/activemq/replica/ReplicaEventSerializer.java
+++ activemq-broker/src/main/java/org/apache/activemq/replica/ReplicaEventSerializer.java
@@ -2,39 +2,22 @@ package org.apache.activemq.replica;
 
 import org.apache.activemq.command.DataStructure;
 import org.apache.activemq.command.Message;
+import org.apache.activemq.openwire.OpenWireFormat;
 import org.apache.activemq.openwire.OpenWireFormatFactory;
 import org.apache.activemq.util.ByteSequence;
 import org.apache.activemq.util.ByteSequenceData;
 import org.apache.activemq.util.IOExceptionSupport;
 import org.apache.activemq.wireformat.WireFormat;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
-import java.util.concurrent.atomic.AtomicLong;
 
 public class ReplicaEventSerializer {
-    private static final Logger logger = LoggerFactory.getLogger(ReplicaEventSerializer.class);
-    private final WireFormat wireFormat = new OpenWireFormatFactory().createWireFormat();
 
-    private final AtomicLong serializeReplicationDataExecutionCounts = new AtomicLong();
-    private final AtomicLong serializeReplicationDataTotalExecutionTime = new AtomicLong();
-    private final AtomicLong serializeMessageDataExecutionCounts = new AtomicLong();
-    private final AtomicLong serializeMessageDataTotalExecutionTime = new AtomicLong();
-    private final AtomicLong deserializeExecutionCounts = new AtomicLong();
-    private final AtomicLong deserializeTotalExecutionTime = new AtomicLong();
+    private final WireFormat wireFormat = new OpenWireFormatFactory().createWireFormat();
 
     byte[] serializeReplicationData(final DataStructure object) throws IOException {
         try {
-            final long startNano = System.nanoTime();
             ByteSequence packet = wireFormat.marshal(object);
-            final byte[] bytes = ByteSequenceData.toByteArray(packet);
-            long serializeCurrentExecution = serializeReplicationDataExecutionCounts.addAndGet(1);
-            long serializeCurrentTotalTime = serializeReplicationDataTotalExecutionTime.addAndGet(System.nanoTime() - startNano);
-            if (serializeCurrentExecution % 1000 == 0) {
-                logger.error("[serializeReplicationData] average execution time for [{}] messages is [{}]ns", serializeCurrentExecution, serializeCurrentTotalTime / serializeCurrentExecution);
-            }
-            return bytes;
+            return ByteSequenceData.toByteArray(packet);
         } catch (IOException e) {
             throw IOExceptionSupport.create("Failed to serialize data: " + object.toString() + " in container: " + e, e);
         }
@@ -42,28 +25,14 @@ public class ReplicaEventSerializer {
 
     byte[] serializeMessageData(final Message message) throws IOException {
         try {
-            final long startNano = System.nanoTime();
             ByteSequence packet = wireFormat.marshal(message);
-            final byte[] bytes = ByteSequenceData.toByteArray(packet);
-            long serializeMessageDataCurrentExecution = serializeMessageDataExecutionCounts.addAndGet(1);
-            long serializeMessageDataCurrentTotalTime = serializeMessageDataTotalExecutionTime.addAndGet(System.nanoTime() - startNano);
-            if (serializeMessageDataCurrentExecution % 1000 == 0) {
-                logger.error("[serializeMessageData] average execution time for [{}] messages is [{}]ns", serializeMessageDataCurrentExecution, serializeMessageDataCurrentTotalTime / serializeMessageDataCurrentExecution);
-            }
-            return bytes;
+            return ByteSequenceData.toByteArray(packet);
         } catch (IOException e) {
             throw IOExceptionSupport.create("Failed to serialize message: " + message.getMessageId() + " in container: " + e, e);
         }
     }
 
     Object deserializeMessageData(final ByteSequence sequence) throws IOException {
-        final long startNano = System.nanoTime();
-        final Object unmarshal = wireFormat.unmarshal(sequence);
-        long deserializeCurrentExecution = deserializeExecutionCounts.addAndGet(1);
-        long deserializeCurrentTotalTime = deserializeTotalExecutionTime.addAndGet(System.nanoTime() - startNano);
-        if (deserializeCurrentExecution % 1000 == 0) {
-            logger.error("[serializeMessageData] average execution time for [{}] messages is [{}]ns", deserializeCurrentExecution, deserializeCurrentTotalTime / deserializeCurrentExecution);
-        }
-        return unmarshal;
+        return wireFormat.unmarshal(sequence);
     }
 }
--- activemq-broker/src/main/java/org/apache/activemq/replica/ReplicaSourceBaseBroker.java
+++ activemq-broker/src/main/java/org/apache/activemq/replica/ReplicaSourceBaseBroker.java
@@ -3,22 +3,15 @@ package org.apache.activemq.replica;
 import org.apache.activemq.broker.Broker;
 import org.apache.activemq.broker.BrokerFilter;
 import org.apache.activemq.broker.ConnectionContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicLong;
 
 public abstract class ReplicaSourceBaseBroker extends BrokerFilter {
-    private static final Logger logger = LoggerFactory.getLogger(ReplicaSourceBaseBroker.class);
     final ReplicaReplicationQueueSupplier queueProvider;
     private ReplicationMessageProducer replicationMessageProducer;
     protected final ReplicaEventSerializer eventSerializer = new ReplicaEventSerializer();
 
     private final AtomicBoolean initialized = new AtomicBoolean();
-
-    private final AtomicLong executionCounts = new AtomicLong();
-    private final AtomicLong totalExecutionTime = new AtomicLong();
 
     ReplicaSourceBaseBroker(Broker next) {
         super(next);
@@ -37,7 +30,6 @@ public abstract class ReplicaSourceBaseBroker extends BrokerFilter {
 
 
     protected void enqueueReplicaEvent(ConnectionContext initialContext, ReplicaEvent event) throws Exception {
-        final long startNano = System.nanoTime();
         if (isReplicaContext(initialContext)) {
             return;
         }
@@ -45,11 +37,6 @@ public abstract class ReplicaSourceBaseBroker extends BrokerFilter {
             return;
         }
         replicationMessageProducer.enqueueReplicaEvent(event);
-        long currentExecution = executionCounts.addAndGet(1);
-        long currentTotalTime = totalExecutionTime.addAndGet(System.nanoTime() - startNano);
-        if (currentExecution % 1000 == 0) {
-            logger.error("[enqueueReplicaEvent] average execution time for [{}] messages is [{}]ns", currentExecution, currentTotalTime / currentExecution);
-        }
     }
 
     protected boolean isReplicaContext(ConnectionContext initialContext) {
