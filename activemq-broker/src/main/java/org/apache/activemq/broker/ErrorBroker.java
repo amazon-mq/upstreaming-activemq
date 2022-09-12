5d83d9d8d23ccb3d9e4a88bfe150c550661964b8
Nikita Shupletsov <nshuplet@amazon.com>
2022-09-12 15:11:12 -0700

    [AMQ-8354] Different implementation of ack replication.

--- activemq-broker/src/main/java/org/apache/activemq/broker/ErrorBroker.java
+++ activemq-broker/src/main/java/org/apache/activemq/broker/ErrorBroker.java
@@ -24,7 +24,6 @@ import java.util.concurrent.ThreadPoolExecutor;
 
 import org.apache.activemq.broker.region.Destination;
 import org.apache.activemq.broker.region.MessageReference;
-import org.apache.activemq.broker.region.QueueMessageReference;
 import org.apache.activemq.broker.region.Subscription;
 import org.apache.activemq.broker.region.virtual.VirtualDestination;
 import org.apache.activemq.command.ActiveMQDestination;
@@ -413,12 +412,7 @@ public class ErrorBroker implements Broker {
     }
 
     @Override
-    public void queueMessageDropped(ConnectionContext context, QueueMessageReference reference) {
-        throw new BrokerStoppedException(this.message);
-    }
-
-    @Override
-    public void topicMessageAcknowledged(ConnectionContext context, Subscription sub, MessageAck ack, MessageReference node) {
+    public void queuePurged(ConnectionContext context, ActiveMQDestination destination) {
         throw new BrokerStoppedException(this.message);
     }
 }
