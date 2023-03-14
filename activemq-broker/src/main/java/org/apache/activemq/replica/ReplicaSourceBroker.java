7182dc59d75258ca389e1528f36da49387a3fbed
Aleksei Eraskin <alekseie@amazon.com>
2023-03-14 16:39:01 -0700

    AMQ-125458 Replication queues purge notification
    cr: https://code.amazon.com/reviews/CR-87338000

--- activemq-broker/src/main/java/org/apache/activemq/replica/ReplicaSourceBroker.java
+++ activemq-broker/src/main/java/org/apache/activemq/replica/ReplicaSourceBroker.java
@@ -49,6 +49,8 @@ import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
+import static org.apache.activemq.replica.ReplicaSupport.*;
+
 public class ReplicaSourceBroker extends ReplicaSourceBaseBroker implements MutativeRoleBroker {
 
     private static final DestinationMapEntry<Boolean> IS_REPLICATED = new DestinationMapEntry<>() {
@@ -535,7 +537,7 @@ public class ReplicaSourceBroker extends ReplicaSourceBaseBroker implements Muta
             return;
         }
 
-        if (ReplicaSupport.MAIN_REPLICATION_QUEUE_NAME.equals(ack.getDestination().getPhysicalName())) {
+        if (MAIN_REPLICATION_QUEUE_NAME.equals(ack.getDestination().getPhysicalName())) {
             replicaSequencer.acknowledge(consumerExchange, ack);
             return;
         }
@@ -629,8 +631,14 @@ public class ReplicaSourceBroker extends ReplicaSourceBaseBroker implements Muta
     @Override
     public void queuePurged(ConnectionContext context, ActiveMQDestination destination) {
         super.queuePurged(context, destination);
+        logIfReplicationQueuePurged(destination);
         replicateQueuePurged(context, destination);
+    }
 
+    private void logIfReplicationQueuePurged(ActiveMQDestination destination) {
+        if(destination.isQueue() && REPLICATION_QUEUE_NAMES.contains(destination.getPhysicalName())) {
+            logger.error("Replication queue was purged {}", destination.getPhysicalName());
+        }
     }
 
     private void replicateQueuePurged(ConnectionContext connectionContext, ActiveMQDestination destination) {
